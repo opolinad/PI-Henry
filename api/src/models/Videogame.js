d5b0853bbbfe19d5fd2661e5eaf5bba5edae707d
Andrés López <andreslop@gmial.com>
2022-02-13 17:38:43 -0500

    first commit, includes .env and install of UUIDV4

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey:true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    released: {
+      type: DataTypes.DATEONLY,
+    },
+    rating: {
+      type: DataTypes.FLOAT,
+    },
+    platforms: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  sequelize.define('genre', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey:true
+    },
+    name: {
+      type: DataTypes.STRING,
+    }
+  });
+};
