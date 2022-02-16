cf2f8fda1b80bf2bf255206d29913935e73d897b
Andrés López <andreslop@gmial.com>
2022-02-16 15:23:07 -0500

    Listo reducers, sin probar

--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,14 +1,27 @@
-import {GET_ALL, GET_BY_GENRE, GET_BY_CONDITION, GET_DETAIL, GET_BY_NAME, ADD_VIDEOGAME} from "../actions";
+import {GET_ALL, GET_BY_GENRE, GET_BY_CONDITION, GET_DETAIL, GET_BY_NAME, GET_ALL_GENRES, ADD_VIDEOGAME} from "../actions";
 
 const initialState = {
     videogames:[],
-    genre:[]
+    genres:[],
+    gameDetail:{}
 }
 
 function reducer (state=initialState, action){
     switch (action.type) {
         case GET_ALL:
-            return {...state, videogames:action.payload}
+            return {...state, videogames:action.payload};
+        case GET_BY_GENRE:
+            return {...state, videogames:action.payload};
+        case GET_BY_CONDITION:
+            return {...state, videogames:action.payload};
+        case GET_DETAIL:
+            return {...state, gameDetail:action.payload};
+        case GET_BY_NAME:
+            return {...state, videogames:action.payload};
+        case GET_ALL_GENRES:
+            return {...state, genres:action.payload};
+        case ADD_VIDEOGAME:
+            return {...state, videogames:action.payload};
         default:
             return state;
     }
