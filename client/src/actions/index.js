d5824bc34fe6b58065c58b89e9531d3a5b0dce20
Andrés López <andreslop@gmial.com>
2022-02-16 14:22:14 -0500

    Listas las acciones (sin probar)

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,25 +6,56 @@ export const GET_DETAIL="GET_DETAIL";
 export const GET_BY_NAME="GET_BY_NAME";
 export const ADD_VIDEOGAME="ADD_VIDEOGAME";
 
-export  function getAllVideogames (){
-    console.log("Entra0");
+export  function getAllVideogames (gamesQty){
     return async (dispatch)=>{
-        let games = await axios.get("http://localhost:3001/videogames",{});
+        let url=gamesQty?`http://localhost:3001/videogames?gamesQty=${gamesQty}`:`http://localhost:3001/videogames`
+        let games = await axios.get(url);
         return dispatch({type:GET_ALL, payload:games.data})
     }
 }
-export function getByGenre (){
-
+export function getByGenre (genre, gamesQty){
+    return async (dispatch)=>{
+        let url=gamesQty?`http://localhost:3001/videogames?gamesQty=${gamesQty}`:`http://localhost:3001/videogames`
+        let games = await axios.get(url);
+        let data=genre?games.data.filter(game=>game.genres.includes(genre)):games.data;
+        return dispatch({type:GET_BY_GENRE, payload:data})
+    }
 }
-export function getByCondition (){
-
+export function getByCondition (condition, gamesQty){
+    return async (dispatch)=>{
+        let url=gamesQty?`http://localhost:3001/videogames?gamesQty=${gamesQty}`:`http://localhost:3001/videogames`;
+        let games = await axios.get(url);
+        let data=condition==="created"?
+        games.data.filter(game=>game.hasOwnProperty("created")):
+        games.data.filter(game=>!game.hasOwnProperty("created"));
+        return dispatch({type:GET_BY_CONDITION, payload:data})
+    }
 }
-export function getDetail (){
-
+export function getDetail (id){
+    return async (dispatch)=>{
+        let gameDetail = await axios.get(`http://localhost:3001/videogame/${id}`);
+        return dispatch({type:GET_DETAIL, payload:gameDetail.data})
+    }
 }
-export function getByName (){
-
+export function getByName (name){
+    return async (dispatch)=>{
+        let games = await axios.get(`http://localhost:3001/videogames/${name}`);
+        return dispatch({type:GET_BY_NAME, payload:games.data})
+    }
 }
-export function addVideogame (){
-
+export function addVideogame (name, description, released, rating, genres, platforms){
+    return async (dispatch)=>{
+        let opt={
+            data:{
+                name,
+                description,
+                released,
+                rating,
+                genres,
+                platforms
+            }
+        }
+        let response = await axios.post(`http://localhost:3001/videogame/`,opt);
+        return dispatch({type:ADD_VIDEOGAME, payload:response.data})
+    }
 }
