b9d58fc088ba8f92d44f50ea473c50a80516ec06
Andrés López <andreslop@gmial.com>
2022-02-25 17:25:16 -0500

    paginación sin bugs

--- client/src/components/Pages/Pages.jsx
+++ client/src/components/Pages/Pages.jsx
@@ -7,28 +7,33 @@ export default function Pages({ resultsPerPage, modifyActualPage,actualPage }) {
     const videogames = useSelector((state) => state.videogamesFilter);
     const numPages = Math.ceil( videogames.length/ resultsPerPage);
     let [renderPage, setRenderPage] = useState(2);
+    let [refreshRender, setRefreshRender] = useState(false);
     useEffect(()=>{
         setRenderPage(2);
+    },[videogames]);
+    useEffect(()=>{
+        setRefreshRender(false);
         renderActualPage();
-    },[videogames, actualPage])
+    },[refreshRender,actualPage,videogames]);
     function onNumberClick(e) {
         modifyActualPage(e.target.value);
     }
     function onBeforeClick() {
+        setRefreshRender(true);
         if ((renderPage - 1) % 3 === 0) {
             return setRenderPage(renderPage -= 2);
         }
         renderPage >= 5 && setRenderPage(renderPage -= 3);
     }
     function onAfterClick() {
+        setRefreshRender(true);
         renderPage + 2 === numPages && setRenderPage(numPages);
         numPages - Number(renderPage) >= 3 && setRenderPage(renderPage += 3);
     }
     function renderActualPage() {
         if (numPages<=1) return;
-        for(let i=1; i<=3;i++){
-            let page=document.getElementById(i);
-            if(page.value==actualPage){
+        for (const page of document.getElementsByClassName("btn-pages")) {
+            if(page?.value==actualPage){
                 page.classList.add("btn-actual-page");
             }else{
                 page.classList.remove("btn-actual-page");
@@ -36,11 +41,14 @@ export default function Pages({ resultsPerPage, modifyActualPage,actualPage }) {
         }
     }
     if (numPages === 1 || numPages === 0) {
+        document.getElementById("cards-container") && (document.getElementById("cards-container").style.height="71.5vh");
         return <div/>;
+    }else{
+        document.getElementById("cards-container") && (document.getElementById("cards-container").style.height="67vh");
     }
     return (<div id="pages-container">
 
-        {renderPage > 2 && <button className="btn-pages" onClick={onBeforeClick}>&lt;</button>}
+        {renderPage > 2 && <button className="btn-nav" onClick={onBeforeClick}>&lt;</button>}
 
         {(renderPage + 1) % 3 === 0 && <button className="btn-pages" id={1} onClick={onNumberClick} value={renderPage - 1}>{renderPage - 1}</button>}
 
@@ -48,7 +56,7 @@ export default function Pages({ resultsPerPage, modifyActualPage,actualPage }) {
 
         {Number(renderPage) + 1 <= numPages && <button className="btn-pages" id={3} onClick={onNumberClick} value={Number(renderPage) + 1}>{Number(renderPage) + 1}</button>}
 
-        {numPages >= renderPage + 2 && <button className="btn-pages" onClick={onAfterClick}>&gt;</button>}
+        {numPages >= renderPage + 2 && <button className="btn-nav" onClick={onAfterClick}>&gt;</button>}
     </div>);
     /*   let pages=[];
       for (let i = 1; i <= numPages; i++) {
